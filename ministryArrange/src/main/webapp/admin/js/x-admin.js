64c5bc2f70c4a892f5c4c22187ba52a3d988b73c
信者得爱 <2291006094@qq.com>
2018-03-13 17:06:49 +0800

    layui2.0版本  tabChange方法有问题  index页更换1.9版本

--- ministryArrange/src/main/webapp/admin/js/x-admin.js
+++ ministryArrange/src/main/webapp/admin/js/x-admin.js
@@ -1,9 +1,8 @@
 layui.use(['element'], function(){
 	$ = layui.jquery;
-  	element = layui.element; 
-  
-  //导航的hover效果、二级菜单等功能，需要依赖element模块
-  // 侧边栏点击隐藏兄弟元素
+	element = layui.element(); 
+	//导航的hover效果、二级菜单等功能，需要依赖element模块
+	// 侧边栏点击隐藏兄弟元素
 	$('.layui-nav-item').click(function(event) {
 		$(this).siblings().removeClass('layui-nav-itemed');
 	});
@@ -34,28 +33,30 @@ layui.use(['element'], function(){
 
 
 
-  	//监听导航点击
-  	element.on('nav(side)', function(elem){
-    	title = elem.find('cite').text();
-    	url = elem.find('a').attr('_href');
-    	// alert(url);
+		//监听导航点击
+		element.on('nav(side)', function(elem){
+		title = elem.find('cite').text();
+		url = elem.find('a').attr('_href');
+		// alert(url);
 
-    	for (var i = 0; i <$('.x-iframe').length; i++) {
-    		if($('.x-iframe').eq(i).attr('src')==url){
-    			element.tabChange('x-tab', i);
-    			return;
-    		}
-    	};
+		for (var i = 0; i <$('.x-iframe').length; i++) {
+			if($('.x-iframe').eq(i).attr('src')==url){
+				element.tabChange('x-tab', i);
+				return;
+			}
+		};
 
-    	res = element.tabAdd('x-tab', {
+		res = element.tabAdd('x-tab', {
 	        title: title//用于演示
 	        ,content: '<iframe frameborder="0" src="'+url+'" class="x-iframe"></iframe>'
 		    });
 
-
 		element.tabChange('x-tab', $('.layui-tab-title li').length-1);
 
-    	$('.layui-tab-title li').eq(0).find('i').remove();
-  });
+		$('.layui-tab-title li').eq(0).find('i').remove();
+	});
+
 });
+
+
 
